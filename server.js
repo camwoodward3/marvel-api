7dd13527d7e0a8f2cf5ba276b8a3284964a569c5
Cameron Woodward <woo23017@byui.edu>
2026-02-12 12:55:14 -0700

    Minor bug changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.log('MongoDB connection error:', err);
